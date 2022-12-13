81c7bb4402117bec9d1974f073a5536315b36cee
thuan-tk1 <thuan2609.tk1@gmail.com>
2022-12-13 07:56:58 +0700

    Config viewBinding

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // setup viewBinding
         setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.pressedBtn.setOnClickListener {
+            Toast.makeText(this, "Click Me", Toast.LENGTH_SHORT).show()
+        }
     }
 }
